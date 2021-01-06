391a03a6d0345d85a46643e3477ffc4d785c5be4
Jena Boehm <boehmjena@gmail.com>
2021-01-06 15:22:12 -0800

    passing post test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,24 +2,31 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 describe('lab-15-auth routes', () => {
   beforeEach(() => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
   });
 
-  it('allows the user to signup via POST', () => {
+  it('allows the user to signup via POST', async() => {
+    // await Promise.all([
+    //   UserService.create({ email: 'test@test.com', password: 'password' }),
+    //   UserService.create({ email: 'test2@test.com', password: 'password2' }),
+    //   UserService.create({ email: 'test3@test.com', password: 'password3' })
+    // ]);
+
     return request(app)
       .post('/api/v1/auth/signup')
       .send({ 
         email: 'test@test.com', 
-        password: 'password' 
+        passwordHash: 'password' 
       })
       .then(res => {
         expect(res.body).toEqual({
           id: expect.any(String),
           email: 'test@test.com',
-          password: 'password'
+          passwordHash: 'password'
         });
       });
   });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,5 +1,7 @@
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE users(
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     email TEXT NOT NULL,
-    password TEXT NOT NULL
+    password_hash TEXT NOT NULL
 );
