aa21ece198d850d6405c7626e2a4568db7bcf597
Jena Boehm <boehmjena@gmail.com>
2021-01-06 14:27:18 -0800

    .post route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,6 +5,22 @@ const app = require('../lib/app');
 
 describe('lab-15-auth routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  it('allows the user to signup via POST', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({ 
+        email: 'test@test.com', 
+        password: 'password' 
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          email: 'test@test.com',
+          password: 'password'
+        });
+      });
   });
 });
