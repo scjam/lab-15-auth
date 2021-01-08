b05971470ff9a8089cee93b92240f8f93184e4a1
ryanleviathan <ryan.m.carreras@gmail.com>
2021-01-08 12:43:49 -0800

    added patch route, test fails

--- __tests__/postGram.test.js
+++ __tests__/postGram.test.js
@@ -133,4 +133,52 @@ describe('lab-15-post gram routes', () => {
          ]
     });
   });
+
+  it('updates a posts caption, only allowing you to do so if you have authorization', async() => {
+    const agent = request.agent(app);
+    const user = await agent
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'test@test.com',
+        password: 'password',
+        profilePhotoURL: 'profile.jpg'
+      });
+
+    let gram = await agent
+      .post('/api/v1/post_grams')
+      .send({ 
+        userId: user.id, 
+        photoURL: 'photo.jpg',
+        caption: 'caption',
+        tags:
+         [ 
+           'tag',
+           'tagged',
+           'tagger'
+         ]
+      });
+
+    gram = await agent
+      .patch(`/api/v1/post_grams/${gram.body.id}`)
+      .send({
+        caption: 'this is my new caption'
+      });
+
+    const res = await agent
+      .get(`/api/v1/post_grams/${gram.body.id}`);
+      
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      comments: expect.anything(),
+      userId: user.body.id, 
+      photoURL: 'photo.jpg',
+      caption: 'caption', 
+      tags:
+         [
+           'tag',
+           'tagged',
+           'tagger'
+         ]
+    });
+  });
 });
